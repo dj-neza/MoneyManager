591df3c14ec0d5983a376b5705c403ff5a678f9c
Neža <neza.djukic@gmail.com>
2016-11-27 18:33:18 +0100

    make it better

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# :moneybag: MoneyPlan(e)t :herb:
+
+**MoneyPlan(e)t** je spletna aplikacija namenjena ljudem, ki imajo težave z urejanjem svojih financ in beleženjem stroškov svojega življenja. Z učinkovitim in enostavnim beleženjem stroškov uporabniku pomaga, da prihrani svoj denar in vidi, za kaj največ zapravlja. Uporabnik lahko svoj denar usmeri v izpolnjevanje večjih ciljev, ki jih lahko ustvari in si nastavi rok za izpolnitev, ter nato dodaja denar. Vsak uporabnik ima na voljo več *denarnic* (denarnica, kreditna kartica, račun na banki...) za katere lahko beleži stroške. Slednje lahko razporeja po kategorijah, lahko pa si nastavi tudi avtomatično ponavljanje stroškov (npr za vsakomesečne račune). Aplikacija omogoča tudi enostavno beleženje izposojenega denarja in posojil drugim ljudem, ter izpis različnih statistik. 
+
+## Ciljna publika in naprave
+Ciljni uporabniki so predvsem ljudje, ki so vajeni uporabe tehnologije in želijo urediti svoje finance in kaj prihraniti. Vseeno je aplikacija preprosta za uporabo, tako da jo lahko uporablja tudi kdo, ki rabi tehnologije ni tako vešč. Aplikacija je primerna za uporabo na vsaki napravi (računalnik, tablica, mobilni telefon), najbolj pa je priročna uporaba na večjem zaslonu (sploh pri pregledu statistik). 
+
+## Različni brskalniki
+
---
+++ js/script.js
@@ -0,0 +1,195 @@
+var users = [];
+var money = [];
+var goals = [];
+var currentUsername = "";
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  	if (localStorage.getItem("users") != null) {
+	    users = JSON.parse(localStorage.getItem("users"));
+	}
+	if (localStorage.getItem("money") != null) {
+	    money = JSON.parse(localStorage.getItem("money"));
+	}
+	if (localStorage.getItem("goals") != null) {
+	    goals = JSON.parse(localStorage.getItem("goals"));
+	}
+	if (localStorage.getItem("currentUsername") != null) {
+	    currentUsername = JSON.parse(localStorage.getItem("currentUsername"));
+	}
+
+	document.getElementById("toggle").onclick = function() {
+		var d = document.getElementById("toggle");
+		var d2 = document.querySelector('.menu_toggle');
+		if(hasClass(d, 'acti')) {
+	        d.className = d.className.replace(/\bacti\b/,'');
+	        fadeOut(d2);
+	    }
+	    else {
+	    	d.className += " acti";
+	    	fadeIn(d2);
+	    }
+	}
+	document.getElementById("toggle2").onclick = function() {
+		var d = document.getElementById("toggle2");
+		var d2 = document.querySelector('.menu_toggle2');
+		if(hasClass(d, 'acti')) {
+	        d.className = d.className.replace(/\bacti\b/,'');
+	        fadeOut(d2);
+	    }
+	    else {
+	    	d.className += " acti";
+	    	fadeIn(d2);
+	    }
+	}
+
+	document.getElementById("exp").onclick = function() { 
+		gumbi("exp", "stat", "inc", "goal"); 
+		openTab("expensecontent", "statscontent", "incomecontent", "goalscontent");
+	}
+	document.getElementById("inc").onclick = function() { 
+		gumbi("inc", "exp", "stat", "goal"); 
+		openTab("incomecontent", "expensecontent", "statscontent", "goalscontent");
+	}
+	document.getElementById("goal").onclick = function() { 
+		gumbi("goal", "exp", "inc", "stat"); 
+		openTab("goalscontent", "expensecontent", "incomecontent", "statscontent");
+	}
+	document.getElementById("stat").onclick = function() { 
+		gumbi("stat", "exp", "inc", "goal"); 
+		openTab("statscontent", "expensecontent", "incomecontent", "goalscontent");
+	}
+});
+
+function gumbi(id1, id2, id3, id4) {
+	document.getElementById(id1).className = document.getElementById(id1).className.replace(/\bneaktivn\b/, "aktiven");
+	document.getElementById(id2).className = document.getElementById(id2).className.replace(/\baktiven\b/, "neaktivn");
+	document.getElementById(id3).className = document.getElementById(id3).className.replace(/\baktiven\b/, "neaktivn");
+	document.getElementById(id4).className = document.getElementById(id4).className.replace(/\baktiven\b/, "neaktivn");
+}
+function openTab(id1, id2, id3, id4) {
+	if (hasClass(document.getElementById(id1), 'neviden')) {
+		document.getElementById(id1).className = document.getElementById(id1).className.replace(/\bneviden\b/,'');
+	}
+	if (!hasClass(document.getElementById(id2), 'neviden')) {
+		document.getElementById(id2).className += " neviden";
+	}
+	if (!hasClass(document.getElementById(id3), 'neviden')) {
+		document.getElementById(id3).className += " neviden";
+	}
+	if (!hasClass(document.getElementById(id4), 'neviden')) {
+		document.getElementById(id4).className += " neviden";
+	}
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+function fadeOut(el){
+  el.style.opacity = 1;
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = 'none';
+      el.classList.add('is-hidden');
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+function fadeIn(el, display){
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function logIn(form) { //TODO - ne dela vec ker ni vezano na formo
+	if (localStorage.getItem("users") != null) {
+	    users = JSON.parse(localStorage.getItem("users"));
+	}
+	for (var i = 0; i < users.length; i++) {
+		if (users[i].email == form.email1.value && users[i].password == form.password1.value) {
+			currentUsername = users[i].username;
+	    	updateDatabaseCurrent(currentUsername);
+			return true;
+		}
+	}
+	alert("Wrong e-mail or password.");
+	return false;
+}
+function signUp(form) { //TODO - ne dela vec ker ni vezano na formo
+	if (form.password2.value == form.password22.value) {
+		var newUser = {
+	      email: form.email2.value,
+	      username: form.username.value,
+	      password: form.password2.value
+	    };
+	    users.push(newUser);
+	    updateDatabaseUsers(users);
+	    currentUsername = users[i].username;
+	    updateDatabaseCurrent(currentUsername);
+	    return true;
+	}
+	else {
+		alert("Passwords don't match.");
+		return false;
+	}
+} 
+function logOut() {
+	var currentUser = "";
+	updateDatabaseCurrent(currentUser);
+}
+
+function updateDatabaseUsers(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+function updateDatabaseMoney(money) {
+    localStorage.setItem("money", JSON.stringify(money));
+}
+function updateDatabaseGoals(goals) {
+    localStorage.setItem("goals", JSON.stringify(goals));
+}
+function updateDatabaseCurrent(username) {
+	localStorage.setItem("currentUsername", JSON.stringify(username));
+}
+
+function div_show(id) {
+	document.getElementById(id).style.display = "block";
+}
+function div_hide(id){
+	document.getElementById(id).style.display = "none";
+}
+
+/* FUNCIONALITY STUFF
+function newTransaction() {
+
+}
+function newExpense() {
+
+}
+function newIncome() {
+
+}
+
+function newGoal() {
+
+}
+function addToGoal() {
+
+}
+
+function menu() {
+
+}
+function user() {
+
+}
+function updateBalance() {
+	
+}
+*/
